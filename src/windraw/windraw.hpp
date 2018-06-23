316d342251317a6bca6aeccc9ab860c5e09fd699
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-06-22 20:21:36 -0700

    Moved windraw.hpp to src directory

---
+++ src/windraw/windraw.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2018 Kosi Nwabueze
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include "windraw/spec.hpp"
+#include "windraw/window.hpp"
+#include "windraw/style.hpp"
