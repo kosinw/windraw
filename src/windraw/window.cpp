38963cb9c9c4132fb6e3aa8799e69b7f66e54517
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-06-22 19:15:55 -0700

    Added windraw/window source + header files

---
+++ src/windraw/window.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2018 Kosi Nwabueze
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#include "windraw/window.hpp"
+
+namespace wd
+{
+
+} // namespace wd
