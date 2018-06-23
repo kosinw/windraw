733828984b9ca7f66b9c87007f5102614221af4d
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-06-22 19:15:31 -0700

    Added example/main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2018 Kosi Nwabueze
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#include <windraw/windraw.hpp>
+#include <iostream>
+
+int main(int, char**)
+{
+	std::cout << "T" << std::endl;
+	
+	return 0;
+}
