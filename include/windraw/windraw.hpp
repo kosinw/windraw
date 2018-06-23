bf06715202aa042d2a15780e3620fd63379424ea
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-06-22 19:15:43 -0700

    Added public includes for windraw

---
+++ include/windraw/windraw.hpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2018 Kosi Nwabueze
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include "windraw/window.hpp"
