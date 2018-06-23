1aec47e012d14a805764b442160c5cdb49606881
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-06-22 20:21:08 -0700

    Removed include/ directory

--- include/windraw/platform.hpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2018 Kosi Nwabueze
-// 
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-#pragma once
--- include/windraw/windraw.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018 Kosi Nwabueze
-// 
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-#pragma once
-
-#include "windraw/window.hpp"
