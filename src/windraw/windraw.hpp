2ea367b54e7848d4c51273e3039adba26ffc1679
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-06-24 15:40:42 -0700

    Fixed windraw.hpp

--- src/windraw/windraw.hpp
+++ src/windraw/windraw.hpp
@@ -6,7 +6,12 @@
 #pragma once
 
 #include <windraw/window/event.hpp>
-#include <windraw/window/math.hpp>
 #include <windraw/window/spec.hpp>
 #include <windraw/window/style.hpp>
 #include <windraw/window/window.hpp>
+
+#include <windraw/util/math.hpp>
+
+#if defined(WD_DIRECT2D_ENABLED)
+#elif defined(WD_GDIPLUS_ENABLED)
+#endif
