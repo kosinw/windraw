c70710304e80baa800a7f42a0a1e4acfad7c2f96
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-06-24 15:40:11 -0700

    Created util directory and added appropriate files

--- src/windraw/util/math.cpp
+++ src/windraw/util/math.cpp
@@ -3,7 +3,7 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-#include <windraw/window/math.hpp>
+#include <windraw/util/math.hpp>
 
 namespace wd
 {
